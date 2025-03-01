d9dae197203a17d25742330de1585deb7de997f3
Ramofvm <ramonfhelipe547@gmail.com>
2025-03-01 15:53:44 -0300

    adição de margem

--- src/Componentes/Carrosel/Carrosel.tsx
+++ src/Componentes/Carrosel/Carrosel.tsx
@@ -5,7 +5,7 @@ import 'swiper/css';
 export function Carrosel() {
 
     return (
-        <div className='mt-6'>
+        <div >
 
             <Swiper
             
@@ -31,34 +31,37 @@ export function Carrosel() {
                       
                     }
                 }}
-            >
+            >     
+
+
+                 
 
                 <SwiperSlide>
-                    <div className="w-60 p-5 border-4 border-transparent hover:border-yellow-500 hover:shadow-lg hover:shadow-yellow-500 transition-all duration-300 ease-in-out">
+                    <div className="w-60 p-5 border-4 m-12 border-transparent hover:border-yellow-500 hover:shadow-lg  hover:shadow-yellow-500 transition-all duration-300 ease-in-out hover:scale-110">
                         <img src="/pikachu.png" alt="pikachu" />
                     </div>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <div className="w-60 p-5 border-4 border-transparent  hover:border-blue-700 hover:shadow-lg hover:shadow-blue-700 transition-all duration-300 ease-in-out">
+                    <div className="w-60 p-5 border-4 m-12 border-transparent  hover:border-blue-700 hover:shadow-lg  hover:shadow-blue-700 transition-all duration-300 ease-in-out hover:scale-110">
                         <img src="/lucario.png" alt="" />
                     </div>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <div className="w-60 p-5 border-4 border-transparent hover:border-red-600 hover:shadow-lg  hover:shadow-red-600 transition-all duration-300 ease-in-out">
+                    <div className="w-60 p-5 border-4 m-12 border-transparent hover:border-red-600 hover:shadow-lg  hover:shadow-red-600 transition-all duration-300 ease-in-out hover:scale-110">
                         <img src="charmander.png" alt="" />
                     </div>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <div className="w-60 p-5 border-4 border-transparent hover:border-green-600 hover:shadow-lg hover:shadow-green-600 transition-all duration-300 ease-in-out">
+                    <div className="w-60 p-5 border-4 m-12 border-transparent hover:border-green-600 hover:shadow-lg  hover:shadow-green-600 transition-all duration-300 ease-in-out hover:scale-110">
                         <img src="rayquaza.png" alt="rayquaza" />
                     </div>
                 </SwiperSlide>
 
                 <SwiperSlide>
-                    <div className=" w-60 p-5 border-4 border-transparent hover:border-purple-600 hover:shadow-lg  hover:shadow-purple-600 duration-300 ease-in-out ">
+                    <div className=" w-60 p-5 border-4 m-12 border-transparent hover:border-purple-600 hover:shadow-lg hover:shadow-purple-600 duration-300 ease-in-out hover:scale-110">
                         <img src="gengar.png" alt="Gengar" />
                     </div>
                 </SwiperSlide>
