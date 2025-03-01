bc949f761f56951e2004c664d0425ba192051603
Ramofvm <ramonfhelipe547@gmail.com>
2025-02-28 21:23:49 -0300

    first commit

---
+++ src/Componentes/Header/Header.tsx
@@ -0,0 +1,12 @@
+export function MyHeader() {
+    return (
+      <header className="m-0 bg-[#F6CF57] p-14 flex flex-col items-center">
+        <img src="/pokebola.png" alt="Pokébola" className="w-24 h-24 mb-6" />
+        
+        <div className="flex">
+          <a href="" className="mx-4 text-2xl font-mono hover:text-red-500 transition-all ease-in duration-200 hover:scale-110">CARTAS</a>
+          <a href="" className="mx-4 text-2xl font-mono hover:text-red-500 transition-all ease-in duration-200 hover:scale-110">INÍCIO</a>
+        </div>
+      </header>
+    );
+  }
